8b49c3d368048cc5c3968c5c75cbeac3bdd0651b
ivan deng <guohuadeng@hotmail.com>
2018-10-31 22:55:46 +0800

    release base chinese

---
+++ app_base_chinese/__manifest__.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+
+# Created on 2018-11-05
+# author: 广州尚鹏，http://www.sunpop.cn
+# email: 300883@qq.com
+# resource of Sunpop
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+# Odoo在线中文用户手册（长期更新）
+# http://www.sunpop.cn/documentation/user/10.0/zh_CN/index.html
+
+# Odoo10离线中文用户手册下载
+# http://www.sunpop.cn/odoo10_user_manual_document_offline/
+# Odoo10离线开发手册下载-含python教程，jquery参考，Jinja2模板，PostgresSQL参考（odoo开发必备）
+# http://www.sunpop.cn/odoo10_developer_document_offline/
+# description:
+
+
+{
+    'name': "App base chinese",
+    'version': '11.0.10.31',
+    'author': 'Sunpop.cn',
+    'category': 'Base',
+    'website': 'http://www.sunpop.cn',
+    'license': 'LGPL-3',
+    'sequence': 2,
+    'summary': """
+    Chinese enhance. Out of the box use in china.
+    Set all chinese default value.
+    Default country, timezone, currency, partner... 
+    """,
+    'description': """
+    odoo Chinese enhance. 中国化增强-基础
+    1. 中文默认值，如国家、时区、货币等。处理模块 base, product.
+    2. 中文演示数据(只有demo模式才加载)
+    """,
+    'pre_init_hook': 'pre_init_hook',
+    'depends': [
+        'product',
+        'l10n_cn'
+    ],
+    'images': [],
+    'data': [
+        'views/product_category_views.xml',
+        'data/ir_value_data.xml',
+        'data/ir_sequence_data.xml',
+        'data/base_data.xml',
+        'data/res_currency_data.xml',
+        'data/product_data.xml',
+    ],
+    'demo': [
+    ],
+    'test': [
+    ],
+    'css': [
+    ],
+    'qweb': [
+    ],
+    'js': [
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': True,
+}
