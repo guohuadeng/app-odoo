b0418bb3f1ad8b687e70507d7be937cdd41c060c
ivan deng <guohuadeng@hotmail.com>
2018-11-26 16:21:01 +0800

    update app with weight

--- app_product_weight_sale/__manifest__.py
+++ app_product_weight_sale/__manifest__.py
@@ -2,20 +2,20 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 {
-    'name' : 'App Sales Order Weight',
-    'version' : '12.0.11.14',
+    'name': 'App Sales Order Weight',
+    'version': '12.0.11.26',
     'summary': 'Add sku weight in Sale Order',
     'sequence': 10,
-    'license':'LGPL-3',
+    'license': 'LGPL-3',
     'description': """
     Add sku weight in Sale Order.
     Calculates total weight of a sale order, which is the sum of individual weights of each unit of the products in the order
     """,
     'category': 'Sales',
-    'author' : 'Sunpop.cn',
-    'website' : 'http://www.sunpop.cn',
+    'author': 'Sunpop.cn',
+    'website': 'http://www.sunpop.cn',
     'images': ['static/description/banner.jpg'],
-    'depends' : ['sale_management'],
+    'depends': ['sale_management'],
     'data': [
         'views/sale_order_views.xml',
     ],
