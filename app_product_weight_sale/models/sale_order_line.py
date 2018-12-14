e64f920d2e84b2245d1af98c73a2520d68938192
ivan deng <guohuadeng@hotmail.com>
2018-12-14 13:06:50 +0800

    opt sale weight.

--- app_product_weight_sale/models/sale_order_line.py
+++ app_product_weight_sale/models/sale_order_line.py
@@ -9,18 +9,20 @@ from odoo import api, fields, models, _
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
-    weight = fields.Float(string='Weight', store=True)
+    # 显示的单位，影响性能暂时不使用
+    # weight_uom_name = fields.Char(string='Weight Measure', related='product_id.weight_uom_id.name', readonly=True)
+    weight = fields.Float(string='Weight', related='product_id.weight', store=True, readonly=True)
     weight_subtotal = fields.Float(string='Weight Subtotal', compute='_compute_weight_subtotal',
                           inverse='_set_weight_subtotal', store=True)
 
     @api.multi
-    @api.depends('product_id', 'product_uom_qty')
+    @api.depends('product_id', 'product_uom', 'product_uom_qty')
     def _compute_weight_subtotal(self):
         for line in self:
-            weight = 0
+            weight_subtotal = 0
             if line.product_id and line.product_id.weight:
-                weight += (line.product_id.weight * line.product_uom_qty / line.product_uom.factor)
-            line.weight = weight
+                weight_subtotal += (line.product_id.weight * line.product_uom_qty / line.product_uom.factor)
+            line.weight_subtotal = weight_subtotal
 
     @api.one
     def _set_weight_subtotal(self):
