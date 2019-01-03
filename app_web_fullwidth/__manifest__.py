549248ded00f3b902b3f5c0751ff218de8ccff72
ivan deng <guohuadeng@hotmail.com>
2019-01-04 01:20:53 +0800

    add purchase split

--- app_web_fullwidth/__manifest__.py
+++ app_web_fullwidth/__manifest__.py
@@ -1,19 +1,25 @@
 # -*- coding: utf-8 -*-
 
-# Created on 2017-11-05
+# Created on 2019-01-04
 # author: 广州尚鹏，http://www.sunpop.cn
 # email: 300883@qq.com
 # resource of Sunpop
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-# Odoo在线中文用户手册（长期更新）
+# Odoo12在线用户手册（长期更新）
+# http://www.sunpop.cn/documentation/user/12.0/en/index.html
+
+# Odoo12在线开发者手册（长期更新）
+# http://www.sunpop.cn/documentation/12.0/index.html
+
+# Odoo10在线中文用户手册（长期更新）
 # http://www.sunpop.cn/documentation/user/10.0/zh_CN/index.html
 
 # Odoo10离线中文用户手册下载
 # http://www.sunpop.cn/odoo10_user_manual_document_offline/
 # Odoo10离线开发手册下载-含python教程，jquery参考，Jinja2模板，PostgresSQL参考（odoo开发必备）
 # http://www.sunpop.cn/odoo10_developer_document_offline/
-# description:
+
 {
     'name': 'App Web Responsive Fullwidth (form view full width)',
     'version': '12.0.1.2',
