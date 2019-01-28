d853558a4ebaba7791f52a9b13eccbdd4b43294f
ivan deng <guohuadeng@hotmail.com>
2019-01-29 01:51:26 +0800

    add weight

--- app_product_weight_sale/__manifest__.py
+++ app_product_weight_sale/__manifest__.py
@@ -2,12 +2,13 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 {
-    'name': 'App Product Weight in Sales Order',
-    'version': '11.19.01.03',
+    'name': 'Weight in Sales Order',
+    'version': '11.19.01.29',
     'summary': 'Add Product sku weight in Sale Order, product weight, sale weight, sale order weight, total weight, kg kg(s) lb lb(s) support',
     'sequence': 10,
     'license': 'LGPL-3',
     'description': """
+    All in one Weight solution for sale, purchase, purchase agreement, stock. Please check follow <strong>Pay</strong> app.
     Add product sku weight in Sale Order. Unit of measure auto weight, kg kg(s) lb lb(s) support. weight sale order.
     Calculates total weight of a sale order, which is the sum of individual weights of each unit of the products in the order。
     Support kg(s) or lb(s)
@@ -15,7 +16,7 @@
     'category': 'Sales',
     'author': 'Sunpop.cn',
     'website': 'http://www.sunpop.cn',
-    'images': ['static/description/banner.jpg'],
+    'images': ['static/description/banner.png'],
     'currency': 'EUR',
     'price': 38,
     'depends': ['sale_management'],
--- app_product_weight_sale/models/sale_order.py
+++ app_product_weight_sale/models/sale_order.py
@@ -15,15 +15,17 @@ class SaleOrder(models.Model):
         else:
             return self.env.ref('uom.product_uom_kgm').name
 
-    weight_total = fields.Float(string='Total Weight', compute='_compute_weight_total')
+    weight = fields.Float(string='Total Weight', compute='_compute_weight')
     # 重量显示的单位
     weight_uom_name = fields.Char(string='Weight Measure', default=_get_default_weight_uom_name, readonly=True)
 
-    def _compute_weight_total(self):
+    @api.multi
+    @api.depends('order_line.weight')
+    def _compute_weight(self):
         for rec in self:
             weight_tot = 0
             for line in rec.order_line:
                 if line.product_id:
-                    weight_tot += line.weight_subtotal or 0.0
-            rec.weight_total = weight_tot
+                    weight_tot += line.weight or 0.0
+            rec.weight = weight_tot
 
