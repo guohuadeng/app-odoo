ba19facc8b95443e82344c36139e725cffc98ed7
ivan deng <guohuadeng@hotmail.com>
2019-03-04 23:03:36 +0800

    chinese opt

--- app_product_weight_sale/__manifest__.py
+++ app_product_weight_sale/__manifest__.py
@@ -3,12 +3,12 @@
 
 {
     'name': 'Weight in Sales Order',
-    'version': '11.19.01.29',
+    'version': '11.19.03.04',
     'summary': 'Add Product sku weight in Sale Order, product weight, sale weight, sale order weight, total weight, kg kg(s) lb lb(s) support',
     'sequence': 10,
     'license': 'LGPL-3',
     'description': """
-    All in one Weight solution for sale, purchase, purchase agreement, stock. Please check follow <strong>Pay</strong> app.
+    All in one Weight solution for sale, purchase, purchase agreement, mrp, stock.
     Add product sku weight in Sale Order. Unit of measure auto weight, kg kg(s) lb lb(s) support. weight sale order.
     Calculates total weight of a sale order, which is the sum of individual weights of each unit of the products in the order。
     Support kg(s) or lb(s)
@@ -18,10 +18,11 @@
     'website': 'http://www.sunpop.cn',
     'images': ['static/description/banner.png'],
     'currency': 'EUR',
-    'price': 38,
+    'price': 68,
     'depends': ['sale_management'],
     'data': [
         'views/sale_order_views.xml',
+        'report/report_sale_order_views.xml',
     ],
     'demo': [
     ],
