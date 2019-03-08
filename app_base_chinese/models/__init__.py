1e402d6e775997fbed81d58d37ae21e831f77149
ivan deng <guohuadeng@hotmail.com>
2019-03-08 14:24:41 +0800

    opt app

--- app_base_chinese/models/__init__.py
+++ app_base_chinese/models/__init__.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
 from . import res_partner
-from . import res_partner_category
 from . import res_currency
 
 
--- app_base_chinese/models/res_partner_category.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import api, models, fields, _
-
-class PartnerCategory(models.Model):
-    _inherit = 'res.partner.category'
-    _order = 'sequence, name'
-
-    sequence = fields.Integer('Sequence', help="Used to order partner category")
