1a8d69ce5287cd826857549f3d2dc6dfde569bf4
ivan deng <guohuadeng@hotmail.com>
2019-04-01 22:39:20 +0800

    update license

--- app_hr_ztree/hooks.py
+++ app_hr_ztree/hooks.py
@@ -4,7 +4,7 @@
 # author: 广州尚鹏，https://www.sunpop.cn
 # email: 300883@qq.com
 # resource of Sunpop
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 # Odoo在线中文用户手册（长期更新）
 # https://www.sunpop.cn/documentation/user/10.0/zh_CN/index.html
