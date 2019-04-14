828ce0191482e87941af830e3eaa3b979ace3e94
ivan deng <guohuadeng@hotmail.com>
2019-04-15 03:04:07 +0800

    fix user chart

--- app_users_chart_hierarchy/__manifest__.py
+++ app_users_chart_hierarchy/__manifest__.py
@@ -23,7 +23,7 @@
 
 {
     'name': 'User Alliance Distribution Chart Hierarchy, 多层级用户',
-    'version': '12.19.04.14',
+    'version': '12.19.04.15',
     'author': 'Sunpop.cn',
     'category': 'Sales',
     'website': 'https://www.sunpop.cn',
