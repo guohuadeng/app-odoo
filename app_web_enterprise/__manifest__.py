d513faa05125f8c34c365d465d4e2d85f5b7ddc5
ivan deng <guohuadeng@hotmail.com>
2019-04-14 18:57:54 +0800

    opt ent color

--- app_web_enterprise/__manifest__.py
+++ app_web_enterprise/__manifest__.py
@@ -18,7 +18,7 @@
 
 {
     'name': "App web enterprise enhance,企业版界面增强",
-    'version': '12.0.9.26',
+    'version': '12.19.04.14',
     'author': 'Sunpop.cn',
     'category': 'Base',
     'website': 'https://www.sunpop.cn',
