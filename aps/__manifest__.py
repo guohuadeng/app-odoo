aede2d3646619c504edec7c6ce0da9d2ecffda2d
ivan deng <guohuadeng@hotmail.com>
2019-04-18 19:29:11 +0800

    add aps

---
+++ aps/__manifest__.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+
+# Created on 2018-11-26
+# author: 广州尚鹏，https://www.sunpop.cn
+# email: 300883@qq.com
+# resource of Sunpop
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+# Odoo12在线用户手册（长期更新）
+# https://www.sunpop.cn/documentation/user/12.0/zh_CN/index.html
+
+# Odoo12在线开发者手册（长期更新）
+# https://www.sunpop.cn/documentation/12.0/index.html
+
+# Odoo10在线中文用户手册（长期更新）
+# https://www.sunpop.cn/documentation/user/10.0/zh_CN/index.html
+
+# Odoo10离线中文用户手册下载
+# https://www.sunpop.cn/odoo10_user_manual_document_offline/
+# Odoo10离线开发手册下载-含python教程，jquery参考，Jinja2模板，PostgresSQL参考（odoo开发必备）
+# https://www.sunpop.cn/odoo10_developer_document_offline/
+# description:
+
+{
+    'name': "APS Advanced planning and scheduling,高级排产",
+    'version': '11.19.04.18',
+    'author': 'Sunpop.cn',
+    'category': 'Base',
+    'website': 'https://www.sunpop.cn',
+    'license': 'LGPL-3',
+    'sequence': 2,
+    'price': 0.00,
+    'currency': 'EUR',
+    'images': ['static/description/banner.png'],
+    'depends': [
+        'mrp',
+    ],
+    'summary': """
+    Normal APS
+    Odoo App of Sunpop.cn
+    """,
+    'description': """
+    模块开发模板，目录结构与文件定义。
+    """,
+    'data': [
+        # 'security/*.xml',
+        # 'views/*.xml',
+        # 'data/*.xml',
+    ],
+    'qweb': [
+        'static/src/xml/*.xml',
+    ],
+    'demo': [],
+    'test': [],
+    'css': [],
+    'js': [],
+    'pre_init_hook': 'pre_init_hook',
+    'post_init_hook': 'post_init_hook',
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
