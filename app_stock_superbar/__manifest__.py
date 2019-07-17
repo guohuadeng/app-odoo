b3996cc5b3f59a7a9c79dc0be67a1106bb9030e6
Ivan.x390 <guohuadeng@hotmail.com>
2019-07-17 14:14:14 +0800

    增加固定表头

--- app_stock_superbar/__manifest__.py
+++ app_stock_superbar/__manifest__.py
@@ -17,7 +17,7 @@
 
 {
     'name': "Stock Superbar ztree, parent children tree",
-    'version': '12.19.06.03',
+    'version': '12.19.07.17',
     'author': 'Sunpop.cn',
     'category': 'Base',
     'website': 'https://www.sunpop.cn',
