2b7d3e11ded14f92ba4960444b6775c287fde86c
ivan deng <guohuadeng@hotmail.com>
2019-10-16 21:03:25 +0800

    update license

--- app_odoo_customize/__manifest__.py
+++ app_odoo_customize/__manifest__.py
@@ -27,7 +27,7 @@
     'author': 'Sunpop.cn',
     'category': 'Productivity',
     'website': 'https://www.sunpop.cn',
-    'license': 'AGPL-3',
+    'license': 'LGPL-3',
     'sequence': 2,
     'summary': """    
     Quick Customize, Debrand, Reset data, Debug. Language Switcher. 
