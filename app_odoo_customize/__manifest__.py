fa9287632c2ba3c40b790ddc10cc721f320ec0db
Ivan Home <guohuadeng@hotmail.com>
2019-10-19 01:30:53 +0800

    update boost

--- app_odoo_customize/__manifest__.py
+++ app_odoo_customize/__manifest__.py
@@ -23,7 +23,7 @@
 
 {
     'name': 'Odoo Customize(Boost,Debranding, My Odoo)',
-    'version': '13.19.10.16',
+    'version': '13.19.10.19',
     'author': 'Sunpop.cn',
     'category': 'Productivity',
     'website': 'https://www.sunpop.cn',
