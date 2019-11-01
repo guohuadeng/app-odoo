cd4f5d8ae092286e727339215a0429c57352f7af
ivan deng <guohuadeng@hotmail.com>
2019-11-01 22:31:31 +0800

    update app-paid

--- app_web_fullwidth/__manifest__.py
+++ app_web_fullwidth/__manifest__.py
@@ -22,7 +22,7 @@
 
 {
     'name': 'App Web Responsive Fullwidth (form view full width)',
-    'version': '13.19.05.30',
+    'version': '13.19.11.01',
     'category': 'web',
     'author': 'Sunpop.cn',
     'website': 'https://www.sunpop.cn',
