09ea4d84c74aa998e6a2379e2f490790bee00c17
ivan deng <guohuadeng@hotmail.com>
2019-12-10 06:23:17 +0800

    fix delivery

--- app_web_enterprise/__manifest__.py
+++ app_web_enterprise/__manifest__.py
@@ -18,7 +18,7 @@
 
 {
     'name': "App web enterprise enhance,企业版界面增强",
-    'version': '13.19.12.09',
+    'version': '13.19.12.10',
     'author': 'Sunpop.cn',
     'category': 'Base',
     'website': 'https://www.sunpop.cn',
--- app_web_enterprise/static/src/js/list_renderer.js
+++ app_web_enterprise/static/src/js/list_renderer.js
@@ -4,13 +4,8 @@ odoo.define('app_web_enterprise.ListRenderer', function (require) {
     var ListRenderer = require('web.ListRenderer');
 
     ListRenderer.include({
-        _renderBody: function () {
-            var self = this;
-            var $rows = this._renderRows();
-            while ($rows.length < 1) {
-                $rows.push(self._renderEmptyRow());
-            }
-            return $('<tbody>').append($rows);
+        _renderEmptyRow: function () {
+            return;
         },
     });
 
