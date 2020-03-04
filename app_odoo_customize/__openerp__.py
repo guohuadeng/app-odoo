deae8329aefa9259b9a13418e466f02948e65e2d
ivan deng <guohuadeng@hotmail.com>
2020-03-04 11:52:18 +0800

    update customize

--- app_odoo_customize/__openerp__.py
+++ app_odoo_customize/__openerp__.py
@@ -15,47 +15,57 @@
 # http://www.sunpop.cn/odoo10_developer_document_offline/
 # description:
 {
-    'name': 'App Odoo Customize(Backend Debranding Title,Language,Documentation,Quick Debug,Delete clear)',
-    'version': '10.0.9.25',
+    'name': 'Customize odoo OEM (Boost, My Odoo)',
+    'version': '10.20.03.04',
     'author': 'Sunpop.cn',
     'category': 'Productivity',
     'website': 'http://www.sunpop.cn',
     'license': 'AGPL-3',
     'sequence': 2,
     'summary': """    
-    Keyword: odoo debrand, odoo debranding, customize my odoo.
-    Quick customize and debranding your own Odoo. Quick debug, Language Switcher, Online Documentation Access,Delete test data.
+    Default keep odoo Logo. For quick developer. Quick customize, set brand, boost, reset data, debug. Language Switcher. 
+    Easy Delete data.reset account chart.
+    customize my odoo. 
     """,
     'description': """
-App Odoo Customize(Debranding Title,Language,Documentation,Quick Debug)
-============
-White label odoo.
-Support Odoo 11, 10, 9.
-Now ready for 12, please email to me. guohuadeng@hotmail.com
-You can config odoo, make it look like your own platform.
-1. Deletes Odoo label in footer
-2. Replaces "Odoo" in Windows title
-3. Customize Documentation, Support, About links and title in usermenu
-4. Adds "Developer mode" link to the top right-hand User Menu.
-5. Adds Quick Language Switcher to the top right-hand User Menu.
-6. Adds Country flags  to the top right-hand User Menu.
-7. Adds English and Chinese user documentation access to the top right-hand User Menu.
-8. Adds developer documentation access to the top right-hand User Menu.
-9. Customize "My odoo.com account" button
-10. Standalone setting panel, easy to setup.
-11. Provide 236 country flags.
-12. Multi-language Support.
-13. Change Powered by Odoo in login screen.(Please change '../views/app_odoo_customize_view.xml' #15)
-14. Quick delete test data in Apps: Sales/POS/Purchase/MRP/Inventory/Accounting/Message/Workflow etc.
-15. Reset All the Sequence to beginning of 1: SO/PO/MO/Invoice...
-16. Fix odoo reload module translation bug while enable english language
-17. Stop Odoo Auto Subscribe(Performance Improve)
-18. Show/Hide Author and Website in Apps Dashboard (odoo 11 only)
-19. Set System Icon (odoo 10 only)
-
-This module can help to white label the Odoo.
-Also helpful for training and support for your odoo end-user.
-The user can get the help document just by one click.
+    App Customize Odoo (Change Title,Language,Documentation,Quick Debug)
+    ============
+    White label odoo.
+    Support Odoo 13, 12, 11, 10, 9.
+    You can config odoo, make it look like your own platform.
+    1. Deletes Odoo label in footer
+    2. Replaces "Odoo" in Windows title
+    3. Customize Documentation, Support, About links and title in usermenu
+    4. Adds "Developer mode" link to the top right-hand User Menu.
+    5. Adds Quick Language Switcher to the top right-hand User Menu.
+    6. Adds Country flags  to the top right-hand User Menu.
+    7. Adds English and Chinese user documentation access to the top right-hand User Menu.
+    8. Adds developer documentation access to the top right-hand User Menu.
+    9. Customize "My odoo.com account" button
+    10. Standalone setting panel, easy to setup.
+    11. Provide 236 country flags.
+    12. Multi-language Support.
+    13. Change Powered by Odoo in login screen.(Please change '../views/app_odoo_customize_view.xml' #15)
+    14. Quick delete test data in Apps: Sales/POS/Purchase/MRP/Inventory/Accounting/Project/Message/Workflow etc.
+    15. Reset All the Sequence to beginning of 1: SO/PO/MO/Invoice...
+    16. Fix odoo reload module translation bug while enable english language
+    17. Stop Odoo Auto Subscribe(Moved to app_odoo_boost)
+    18. Show/Hide Author and Website in Apps Dashboard
+    19. One Click to clear all data (Sometime pls click twice)
+    20. Show quick upgrade in app dashboard, click to show module info not go to odoo.com
+    21. Can clear and reset account chart. Be cautious
+    22. Update online manual and developer document to odoo12
+    23. Add reset or clear website blog data
+    24. Customize Odoo Native Module(eg. Enterprise) Url
+    25. Add remove expense data
+    26. Add multi uninstall modules
+    27. Add odoo boost modules link.
+    28. Easy Menu manager.
+    29. Add Install version in App list. Add Local updatable filter in app list.
+    
+    This module can help to white label the Odoo.
+    Also helpful for training and support for your odoo end-user.
+    The user can get the help document just by one click.
 
     """,
     'images': ['static/description/banner.png'],
