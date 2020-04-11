12d196f80680b4f8ceb3c9fa1aac33628ea80c73
ivan deng <guohuadeng@hotmail.com>
2020-04-11 23:25:30 +0800

    add remove refferal

--- app_odoo_customize/__manifest__.py
+++ app_odoo_customize/__manifest__.py
@@ -23,7 +23,7 @@
 
 {
     'name': 'Customize odoo OEM (Boost, My Odoo)',
-    'version': '13.20.03.26',
+    'version': '13.20.04.11',
     'author': 'Sunpop.cn',
     'category': 'Productivity',
     'website': 'https://www.sunpop.cn',
@@ -71,10 +71,45 @@
     28. Easy Menu manager.
     29. Apps version compare. Add Install version in App list. Add Local updatable filter in app list.
     30. 1 key export app translate file like .po file.
+    31. Show or hide odoo Referral in the top menu.
     
     This module can help to white label the Odoo.
     Also helpful for training and support for your odoo end-user.
     The user can get the help document just by one click.
+    ## 在符合odoo开源协议的前提下，去除odoo版权信息，自定义你的odoo
+    可完全自行设置下列 odoo 选项，让 odoo 看上去像是你的软件产品
+    支持Odoo 13,12, 11, 10, 9 版本，社区版企业版通用    
+    1. 删除菜单导航页脚的 Odoo 标签
+    2. 将弹出窗口中 "Odoo" 设置为自定义名称
+    3. 自定义用户菜单中的 Documentation, Support, About 的链接
+    4. 在用户菜单中增加快速切换开发模式
+    5. 在用户菜单中增加快速切换多国语言
+    6. 对语言菜单进行美化，设置国旗图标
+    7. 在用户菜单中增加中/英文用户手册，可以不用翻墙加速了
+    8. 在用户菜单中增加开发者手册，含python教程，jquery参考，Jinja2模板，PostgresSQL参考
+    9. 在用户菜单中自定义"My odoo.com account"
+    10. 单独设置面板，每个选项都可以自定义
+    11. 提供236个国家的国旗文件（部份需要自行设置文件名）
+    12. 多语言版本
+    13. 自定义登陆界面中的 Powered by Odoo
+    14. 快速删除测试数据，支持模块包括：销售/POS门店/采购/生产/库存/会计/项目/消息与工作流等.
+    15. 将各类单据的序号重置，从1开始，包括：SO/PO/MO/Invoice 等
+    16. 修复odoo启用英文后模块不显示中文的Bug
+    17. 可停用odoo自动订阅功能，避免“同样对象关注2次”bug，同时提升性能
+    18. 显示/隐藏应用的作者和网站-在应用安装面板中
+    19. 一键清除所有数据（视当前数据情况，有时需点击2次）
+    20. 在应用面板显示快速升级按键，点击时不会导航至 odoo.com
+    21. 清除并重置会计科目表
+    22. 全新升级将odoo12用户及开发手册导航至国内网站，或者自己定义的网站
+    23. 增加清除网站数据功能
+    24. 自定义 odoo 原生模块跳转的url(比如企业版模块)
+    25. 增加删除费用报销数据功能
+    26. 增加批量卸载模块功能
+    27. 增加odoo加速功能
+    28. 快速管理顶级菜单
+    29. App版本比较，快速查看可本地更新的模块
+    30. 一键导出翻译文件 po
+    31. 显示或去除 odoo 推荐
     """,
     'images': ['static/description/banner.gif'],
     'depends': [
