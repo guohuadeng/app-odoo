95b24c350cc3ae7ee76ea8a28ce4644ffc42d77f
ivan deng <guohuadeng@hotmail.com>
2020-04-11 19:06:59 +0800

    fix hatchtec

--- l10n_cn_standard_latest/__manifest__.py
+++ l10n_cn_standard_latest/__manifest__.py
@@ -9,7 +9,7 @@
 
 {
     'name': '2020最新中国企业会计表.Latest Chinese Accounting.',
-    'version': '13.20.03.23',
+    'version': '13.20.04.11',
     'author': 'Sunpop.cn',
     'category': 'Localization',
     'website': 'https://www.sunpop.cn',
