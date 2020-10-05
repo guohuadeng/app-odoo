8b4c129efc7ea848c38d0132edbec0245497c873
ivan deng <guohuadeng@hotmail.com>
2020-10-05 20:58:16 +0800

    clear master

--- app_odoo_customize/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-
-import ir_ui_view
-import app_theme_config_settings
-import base_language_install
-import mail_thread
--- app_ui_enhance/models/__init__.py
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf-8 -*-
-import app_ui_config_settings
-import report
