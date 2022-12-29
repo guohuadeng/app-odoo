d5677511604101c193779781e8fa08db2e0482d6
Chill <chill421@qq.com>
2022-12-29 18:16:32 +0800

    fix #I67W2P app_odoo_customize,清空业务数据可用

--- app_odoo_customize/__manifest__.py
+++ app_odoo_customize/__manifest__.py
@@ -23,7 +23,7 @@
 
 {
     'name': 'odoo 16 Customize OEM(Boost, Data reset)',
-    'version': '16.22.10.21',
+    'version': '16.22.12.29',
     'author': 'Sunpop.cn',
     'category': 'Productivity',
     'website': 'https://www.sunpop.cn',
