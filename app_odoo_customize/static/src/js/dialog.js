f4a37559ec2c5a62fdd6db0133a698dd802eb222
ivan deng <guohuadeng@hotmail.com>
2023-02-26 00:02:14 +0800

    fix superbar

--- app_odoo_customize/static/src/js/dialog.js
+++ app_odoo_customize/static/src/js/dialog.js
@@ -1,24 +1,26 @@
 /** @odoo-module **/
 
-import { Dialog } from "@web/core/dialog/dialog";
-import { patch } from "@web/core/utils/patch";
-import { session } from "@web/session";
+import {Dialog} from "@web/core/dialog/dialog";
+import {patch} from "@web/core/utils/patch";
+import {session} from "@web/session";
 
 patch(Dialog.prototype, "app_odoo_customize.Dialog", {
     setup() {
         this._super.apply(this, arguments);
         const app_system_name = session.app_system_name || "odooApp";
         this.title = app_system_name;
-        owl.onMounted(() => {
-            this.setDrag();
-        });
     },
-    setDrag() {
-        var $dl = $('#' + this.id + ' .modal-dialog .modal-content');
+    mounted() {
+        //todo: 不生效
+        this._super.apply(this, arguments);
+        let self = this;
+        var $dl = $('#' + self.id + ' .modal-dialog .modal-content');
         if ($dl)
-            $dl.draggable({
-                handle: ".modal-header"
-            });
+            setTimeout(function () {
+                $dl.draggable({
+                    handle: ".modal-header"
+                });
+            }, 800);
     },
 });
 
