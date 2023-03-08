987f134e236e4501dce153289005db2e4f0956fd
ivan deng <guohuadeng@hotmail.com>
2023-03-09 04:53:18 +0800

    fix misc

--- app_odoo_customize/__manifest__.py
+++ app_odoo_customize/__manifest__.py
@@ -23,7 +23,7 @@
 
 {
     'name': 'odoo 16 Customize OEM(Boost, Data reset)',
-    'version': '16.23.02.26',
+    'version': '16.23.03.09',
     'author': 'Sunpop.cn',
     'category': 'Productivity',
     'website': 'https://www.sunpop.cn',
