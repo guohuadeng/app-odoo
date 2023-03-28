01fc6921b5c4657b3f83dddad0c324351661d374
ivan deng <guohuadeng@hotmail.com>
2023-03-29 02:34:20 +0800

    opt chatgpt

--- app_chatgpt/models/ai_robot.py
+++ app_chatgpt/models/ai_robot.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-import requests
+import requests,json
 import openai
 from odoo import api, fields, models, _
 from odoo.exceptions import UserError
@@ -44,8 +44,74 @@ GPT-3	A set of models that can understand and generate natural language
 
     def action_disconnect(self):
         requests.delete('https://chatgpt.com/v1/disconnect')
+        
+    def get_ai(self, data, partner_name='odoo', *args):
+        #     通用方法
+        self.ensure_one()
+        if hasattr(self, 'get_%s' % self.provider):
+            return getattr(self, '%get_' % self.provider)(data, partner_name, *args)
 
-    def get_openai(self, data):
+    def get_openai(self, data, partner_name='odoo', *args):
+        self.ensure_one()
+        headers = {"Content-Type": "application/json", "Authorization": f"Bearer {self.openapi_api_key}"}
+        R_TIMEOUT = 300
+        o_url = self.endpoint or "https://api.openai.com/v1/chat/completions"
+    
+        # 以下处理 open ai
+        #     获取模型信息
+        # list_model = requests.get("https://api.openai.com/v1/models", headers=headers)
+        # model_info = requests.get("https://api.openai.com/v1/models/%s" % ai_model, headers=headers)
+        if self.ai_model == 'dall-e2':
+            # todo: 处理 图像引擎，主要是返回参数到聊天中
+            # image_url = response['data'][0]['url']
+            # https://platform.openai.com/docs/guides/images/introduction
+            pdata = {
+                "prompt": data,
+                "n": 3,
+                "size": "1024x1024",
+            }
+            return '建设中'
+        elif self.ai_model in ['gpt-3.5-turbo', 'gpt-3.5-turbo-0301']:
+            pdata = {
+                "model": self.ai_model,
+                "messages": [{"role": "user", "content": data}],
+                "temperature": 0.9,
+                "max_tokens": self.max_length or 1000,
+                "top_p": 1,
+                "frequency_penalty": 0.0,
+                "presence_penalty": 0.6,
+                "user": partner_name,
+                "stop": ["Human:", "AI:"]
+            }
+            _logger.warning('=====================open input pdata: %s' % pdata)
+            response = requests.post(o_url, data=json.dumps(pdata), headers=headers, timeout=R_TIMEOUT)
+            res = response.json()
+            if 'choices' in res:
+                # for rec in res:
+                #     res = rec['message']['content']
+                res = '\n'.join([x['message']['content'] for x in res['choices']])
+                return res
+        else:
+            pdata = {
+                "model": self.ai_model,
+                "prompt": data,
+                "temperature": 0.9,
+                "max_tokens": self.max_length or 1000,
+                "top_p": 1,
+                "frequency_penalty": 0.0,
+                "presence_penalty": 0.6,
+                "user": partner_name,
+                "stop": ["Human:", "AI:"]
+            }
+            response = requests.post(o_url, data=json.dumps(pdata), headers=headers, timeout=R_TIMEOUT)
+            res = response.json()
+            if 'choices' in res:
+                res = '\n'.join([x['text'] for x in res['choices']])
+                return res
+    
+        return "获取结果超时，请重新跟我聊聊。"
+
+    def get_azure(self, data, partner_name='odoo', *args):
         self.ensure_one()
         # only for azure
         openai.api_type = self.provider
