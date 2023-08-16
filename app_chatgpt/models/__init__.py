5c8ee32f318158173e8d191a974796dbe22025a8
Chill <chill421@qq.com>
2023-08-16 10:44:39 +0800

    基础调整

--- app_chatgpt/models/__init__.py
+++ app_chatgpt/models/__init__.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
+from . import res_partner
 from . import mail_channel
 from . import res_config_settings
 from . import ai_robot
-from . import res_partner
 from . import res_partner_ai_use
 from . import res_users
 from . import mail_message
