db2fb1334327d5a8b56a341cf51772a91a822616
Ivan Office <guohuadeng@hotmail.com>
2023-08-18 05:30:39 +0800

    update doc

--- app_chatgpt/models/mail_channel.py
+++ app_chatgpt/models/mail_channel.py
@@ -263,11 +263,13 @@ class Channel(models.Model):
             # 非4版本，取0次。其它取3 次历史
             chat_count = 3
             if '4' in ai.ai_model:
+                chat_count = 1
                 if hasattr(self, 'chat_count'):
                     if self.chat_count > 0:
                         chat_count = 1
             else:
                 chat_count = chat_count
+                
             if author_id != answer_id.id and self.channel_type == 'chat':
                 # 私聊
                 _logger.info(f'私聊:author_id:{author_id},partner_chatgpt.id:{answer_id.id}')
