2c4c50b311f4ea10a95fe8dfe427c770266ddced
Chill <chill421@qq.com>
2023-09-27 12:47:31 +0800

    upgrade chatgpt translator.

--- app_chatgpt/__manifest__.py
+++ app_chatgpt/__manifest__.py
@@ -10,7 +10,7 @@
 
 {
     'name': 'ChatGPT4, China Ali,AiGC Center.Ai服务中心，聚合全网Ai',
-    'version': '16.23.09.24',
+    'version': '16.23.09.27',
     'author': 'odooai.cn',
     'company': 'odooai.cn',
     'maintainer': 'odooai.cn',
