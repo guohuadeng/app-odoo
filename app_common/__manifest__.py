72c740dd76007b695f73ce6b733ce27051fab082
Ivan Office <guohuadeng@hotmail.com>
2023-09-30 10:58:00 +0800

    int app-odoo 17, paid

---
+++ app_common/__manifest__.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+
+# Created on 2023-02-02
+# author: 欧度智能，https://www.odooai.cn
+# email: 300883@qq.com
+# resource of odooai
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+# Odoo16在线用户手册（长期更新）
+# https://www.odooai.cn/documentation/16.0/zh_CN/index.html
+
+# Odoo16在线开发者手册（长期更新）
+# https://www.odooai.cn/documentation/16.0/zh_CN/developer.html
+
+# Odoo13在线用户手册（长期更新）
+# https://www.odooai.cn/documentation/user/13.0/zh_CN/index.html
+
+# Odoo13在线开发者手册（长期更新）
+# https://www.odooai.cn/documentation/13.0/index.html
+
+# Odoo10在线中文用户手册（长期更新）
+# https://www.odooai.cn/documentation/user/10.0/zh_CN/index.html
+
+# Odoo10离线中文用户手册下载
+# https://www.odooai.cn/odoo10_user_manual_document_offline/
+# Odoo10离线开发手册下载-含python教程，jquery参考，Jinja2模板，PostgresSQL参考（odoo开发必备）
+# https://www.odooai.cn/odoo10_developer_document_offline/
+
+##############################################################################
+#    Copyright (C) 2009-TODAY odooai.cn Ltd. https://www.odooai.cn
+#    Author: Ivan Deng，300883@qq.com
+#    You can modify it under the terms of the GNU LESSER
+#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
+#    See <http://www.gnu.org/licenses/>.
+#
+#    It is forbidden to publish, distribute, sublicense, or sell copies
+#    of the Software or modified copies of the Software.
+##############################################################################
+
+{
+    'name': "odooai Odooapp Common Func",
+    'version': '16.5.23.09.27',
+    'author': 'odooai.cn',
+    'category': 'Base',
+    'website': 'https://www.odooai.cn',
+    'live_test_url': 'https://demo.odooapp.cn',
+    'license': 'LGPL-3',
+    'sequence': 2,
+    'price': 0.00,
+    'currency': 'EUR',
+    'images': ['static/description/banner.png'],
+    'summary': '''
+    Core for common use odooai apps.
+    基础核心，必须没有要被依赖字段及视图等，实现auto_install
+    ''',
+    'description': '''    
+    Support Odoo 16, Enterprise and Community Edition
+    need to setup odoo.conf, add follow:
+    server_wide_modules = web,app_common
+    1. 
+    2. 
+    3. Multi-language Support.
+    4. Multi-Company Support.
+    5. Support Odoo 16, Enterprise and Community Edition
+    ==========
+    1. 
+    2. 
+    3. 多语言支持
+    4. 多公司支持
+    5. Odoo 16, 企业版，社区版，多版本支持
+    ''',
+    'depends': [
+        'mail',
+        'web',
+    ],
+    'data': [
+        'views/ir_cron_views.xml',
+        # 'report/.xml',
+    ],
+    'qweb': [
+        'static/src/xml/*.xml',
+    ],
+    'demo': [],
+    # 'pre_init_hook': 'pre_init_hook',
+    # 'post_init_hook': 'post_init_hook',
+    # 'uninstall_hook': 'uninstall_hook',
+    'installable': True,
+    'application': True,
+    'auto_install': True,
+}
