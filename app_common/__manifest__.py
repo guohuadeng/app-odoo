c547b92aa98fcb330efdc3a2b6f1378424d3e707
Chill <chill421@qq.com>
2023-11-15 14:30:10 +0800

    Create attachments without setting website_id

--- app_common/__manifest__.py
+++ app_common/__manifest__.py
@@ -39,7 +39,7 @@
 
 {
     'name': "odooAi Common Util and Tools,欧度智能基础核心优化",
-    'version': '16.23.11.12',
+    'version': '16.23.11.15',
     'author': 'odooai.cn',
     'category': 'Base',
     'website': 'https://www.odooai.cn',
