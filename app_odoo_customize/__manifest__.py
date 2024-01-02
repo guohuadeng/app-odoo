092e3256e80cdb0312662b169fba4feb3ac24a7b
Ivan Office <guohuadeng@hotmail.com>
2024-01-02 20:52:55 +0800

    update misc

--- app_odoo_customize/__manifest__.py
+++ app_odoo_customize/__manifest__.py
@@ -23,7 +23,7 @@
 
 {
     'name': 'odoo17 Tweak,Ai Employee,Boost,Customize All in One. Customize,UI,Boost,Security,Data',
-    'version': '16.23.11.28',
+    'version': '16.24.01.02',
     'author': 'odooai.cn',
     'category': 'Extra Tools',
     'website': 'https://www.odooai.cn',
