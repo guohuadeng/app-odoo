d4a6febabc54820a510f62762a304da59b04a614
Ivan Office <guohuadeng@hotmail.com>
2024-02-26 14:20:30 +0800

    fix 17 hooks

--- app_odoo_customize/hooks.py
+++ app_odoo_customize/hooks.py
@@ -18,21 +18,21 @@
 from odoo import api, SUPERUSER_ID, _
 
 
-def pre_init_hook(cr):
+def pre_init_hook(env):
     try:
         # 更新企业版指向
         sql = "UPDATE ir_module_module SET website = '%s' WHERE license like '%s' and website <> ''" % ('https://www.odooai.cn', 'OEEL%')
-        cr.execute(sql)
-        cr.commit()
+        env.cr.execute(sql)
+        env.cr.commit()
     except Exception as e:
         pass
 
-def post_init_hook(cr):
+def post_init_hook(env):
     # a = check_module_installed(cr, ['app_web_superbar','aaaaa'])
     pass
     # cr.execute("")
 
-def uninstall_hook(cr, registry):
+def uninstall_hook(env):
     """
     数据初始化，卸载时执行
     """
--- l10n_cn_standard_latest/hooks.py
+++ l10n_cn_standard_latest/hooks.py
@@ -17,14 +17,14 @@
 
 from odoo import api, SUPERUSER_ID
 
-def pre_init_hook(cr):
+def pre_init_hook(env):
     """
     数据初始化，只在安装时执行，更新时不执行
     """
     pass
 
 
-def post_init_hook(cr, registry):
+def post_init_hook(env):
     """
     数据初始化，只在安装后执行，更新时不执行
     此处不执行，只是记录，该数据已处理完成
@@ -32,8 +32,8 @@ def post_init_hook(cr, registry):
     # cr.execute("UPDATE account_account_template set group_id = "
     #            "(select id from account_group where account_group.code_prefix_start=trim(substring(account_account_template.code from 1 for 1)) limit 1);")
 
-    cr.execute("UPDATE account_account set group_id = "
+    env.cr.execute("UPDATE account_account set group_id = "
                "(select id from account_group where account_group.code_prefix_start=trim(substring(account_account.code from 1 for 1)) limit 1);")
-    cr.commit()
+    env.cr.commit()
     pass
 
