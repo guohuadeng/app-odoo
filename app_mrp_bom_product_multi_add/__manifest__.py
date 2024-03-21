5b1aa8b380c22baffa26764f82c8915e3723cdc3
Chill <chill421@qq.com>
2024-03-21 17:12:47 +0800

    [MIG] app_mrp_bom_product_multi_add: Migration to 17.0

---
+++ app_mrp_bom_product_multi_add/__manifest__.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+
+# Created on 2018-08-15
+# author: 欧度智能，https://www.odooai.cn
+# email: 300883@qq.com
+# resource of odooai
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+# Odoo16在线用户手册（长期更新）
+# https://www.odooai.cn/documentation/16.0/zh_CN/index.html
+
+# Odoo16在线开发者手册（长期更新）
+# https://www.odooai.cn/documentation/16.0/zh_CN/developer.html
+
+# Odoo13在线用户手册（长期更新）
+# https://www.odooai.cn/documentation/user/13.0/zh_CN/index.html
+
+# Odoo13在线开发者手册（长期更新）
+# https://www.odooai.cn/documentation/13.0/index.html
+
+# Odoo在线中文用户手册（长期更新）
+# https://www.odooai.cn/documentation/user/10.0/zh_CN/index.html
+
+# Odoo10离线中文用户手册下载
+# https://www.odooai.cn/odoo10_user_manual_document_offline/
+# Odoo10离线开发手册下载-含python教程，jquery参考，Jinja2模板，PostgresSQL参考（odoo开发必备）
+# https://www.odooai.cn/odoo10_developer_document_offline/
+# description:
+
+{
+    'name': "App MRP Bom Product Multi Batch Add",
+    'version': '24.03.21',
+    'author': 'odooai.cn',
+    'category': 'Base',
+    'website': 'https://www.odooai.cn',
+    'live_test_url': 'https://demo.odooapp.cn',
+    'license': 'LGPL-3',
+    'sequence': 2,
+    'price': 0.00,
+    'currency': 'USD',
+    'summary': """
+    App MRP Bom Product Multi Batch Add, 制造Bom批量增加产品.
+    Odoo App of odooai.cn
+    """,
+    'description': """
+    App MRP Bom Product Multi Add. 
+    1. One Click to add multi product to MRP Bom.
+    2. All the product can filter and group.
+    制造Bom批量增加产品
+    1. 可以一键快速将多个产品加到制造Bom中
+    2. 可对产品进行过滤、分组，然后批量加入
+    """,
+    'depends': [
+        # 'app_web_one2many_multi_add',
+        'mrp',
+    ],
+    'images': ['static/description/mrp1.gif'],
+    'data': [
+        'views/mrp_bom_views.xml',
+    ],
+    'demo': [
+    ],
+    'test': [
+    ],
+    'css': [
+    ],
+    'qweb': [
+    ],
+    'js': [
+    ],
+    'post_load': None,
+    'post_init_hook': None,
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
