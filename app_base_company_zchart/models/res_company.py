1a617e05b44abbbe2b4401c33ed0207cc95f11df
Ivan Office <guohuadeng@hotmail.com>
2024-05-25 00:40:59 +0800

    opt wl_base, misc

--- app_base_company_zchart/models/res_company.py
+++ app_base_company_zchart/models/res_company.py
@@ -10,7 +10,7 @@ class Company(models.Model):
     parent_path = fields.Char(index=True, unaccent=False)
     
     # 注意，res.partner 有 parent_id 和 child_ids
-    all_child_ids = fields.One2many('res.company', string='Child Companies', compute='_compute_all_child_ids')
+    all_child_ids = fields.One2many('res.company', string='All Child Companies', compute='_compute_all_child_ids')
     
     @api.depends('parent_id', 'child_ids')
     def _compute_all_child_ids(self):
