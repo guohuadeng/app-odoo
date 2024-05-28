2afa8f36318f185abd70f6cf090a93cf5409ab27
Chill <chill421@qq.com>
2024-05-28 14:32:40 +0800

    #I9SLZK [l10n_cn_standard_latest]增加预收预付的科目

--- l10n_cn_standard_latest/__manifest__.py
+++ l10n_cn_standard_latest/__manifest__.py
@@ -9,7 +9,7 @@
 
 {
     'name': '2023最新中国会计科目表.企业标准会计.Latest Chinese Accounting for odoo16',
-    'version': '16.24.05.09',
+    'version': '16.24.05.28',
     'author': 'odooai.cn',
     'category': 'Accounting/Localizations/Account Charts',
     'website': 'https://www.odooai.cn',
