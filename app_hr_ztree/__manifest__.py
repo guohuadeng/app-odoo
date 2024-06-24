96f1993acec2329a0d4366ca5e231131c8e78823
Ivan Office <guohuadeng@hotmail.com>
2024-06-24 20:03:46 +0800

    update ztree relate

---
+++ app_hr_ztree/__manifest__.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+
+# Created on 2023-10-23
+# author: 欧度智能，https://www.odooai.cn
+# email: 300883@qq.com
+# resource of odooai
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+# Odoo16在线用户手册（长期更新）
+# https://www.odooai.cn/documentation/16.0/zh_CN/index.html
+
+# Odoo16在线开发者手册（长期更新）
+# https://www.odooai.cn/documentation/16.0/zh_CN/developer.html
+
+# Odoo13在线用户手册（长期更新）
+# https://www.odooai.cn/documentation/user/13.0/zh_CN/index.html
+
+# Odoo13在线开发者手册（长期更新）
+# https://www.odooai.cn/documentation/13.0/index.html
+
+# Odoo在线中文用户手册（长期更新）
+# https://www.odooai.cn/documentation/user/10.0/zh_CN/index.html
+
+# Odoo10离线中文用户手册下载
+# https://www.odooai.cn/odoo10_user_manual_document_offline/
+# Odoo10离线开发手册下载-含python教程，jquery参考，Jinja2模板，PostgresSQL参考（odoo开发必备）
+# https://www.odooai.cn/odoo10_developer_document_offline/
+# description:
+
+{
+    'name': "App HR department ztree, parent children tree",
+    'version': '24.06.13',
+    'author': 'odooai.cn',
+    'category': 'Extra tools',
+    'website': 'https://www.odooai.cn',
+    'live_test_url': 'https://demo.odooapp.cn',
+    'license': 'LGPL-3',
+    'sequence': 2,
+    'summary': """
+    Use for parent children tree list select navigator. hr department employee tree.
+    ztree widget.
+    """,
+    'description': """
+    zTree widget.
+    Advance search with real parent children tree, ListView or KanbanView ,
+    eg: Product category tree ,Department tree, stock location tree.
+    超级方便的查询，树状视图。
+    """,
+    'price': 0.00,
+    'currency': 'EUR',
+    'depends': [
+        'hr',
+    ],
+    'images': ['static/description/banner.gif'],
+    'data': [
+        'views/hr_views.xml',
+    ],
+    'demo': [
+    ],
+    'test': [
+    ],
+    'css': [
+    ],
+    'qweb': [
+        'static/src/xml/*.xml',
+    ],
+    'js': [
+    ],
+    'post_load': None,
+    'post_init_hook': 'post_init_hook',
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
