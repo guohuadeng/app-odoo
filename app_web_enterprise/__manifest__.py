fe4b54ff1cce4870d79cb7f66211b658e8d472c0
Ivan Office <guohuadeng@hotmail.com>
2024-07-17 17:41:44 +0800

    update theme

--- app_web_enterprise/__manifest__.py
+++ app_web_enterprise/__manifest__.py
@@ -18,7 +18,7 @@
 
 {
     'name': "odoo Enterprise enhance Pack,企业版界面及操作增强",
-    'version': '16.23.10.26',
+    'version': '16.24.07.17',
     'author': 'odooai.cn',
     'category': 'Extra tools',
     'website': 'https://www.odooai.cn',
