912eb149673d13903754000f44145aa3a63eb7be
Ivan Office <guohuadeng@hotmail.com>
2024-08-14 21:28:02 +0800

    fix notify

--- app_base_chinese/__manifest__.py
+++ app_base_chinese/__manifest__.py
@@ -18,7 +18,7 @@
 
 {
     'name': 'odoo中文版套件之基础,中国会计基础,Chinese Enhance All in One，',
-    'version': '24.06.21',
+    'version': '24.08.14',
     'author': 'odooai.cn',
     'category': 'Base',
     'website': 'https://www.odooai.cn',
