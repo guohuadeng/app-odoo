d894d930493eebbe847e4dadf3e6d5b9e1206bb0
Ivan Office <guohuadeng@hotmail.com>
2024-08-26 00:47:27 +0800

    update po relate

--- app_common/__manifest__.py
+++ app_common/__manifest__.py
@@ -39,7 +39,7 @@
 
 {
     'name': "odooAi Common Util and Tools,欧度智能基础核心优化",
-    'version': '16.24.08.08',
+    'version': '16.24.08.26',
     'author': 'odooai.cn',
     'category': 'Extra tools',
     'website': 'https://www.odooai.cn',
