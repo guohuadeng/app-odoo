2ca4c422b0059a533ae6aa642f7d1e07da925648
Chill <chill421@qq.com>
2024-09-06 16:16:47 +0800

    fix #IAP7B9 【l10n_cn_standard_latest】进项税，也增加（含税价）处理

--- l10n_cn_standard_latest/__manifest__.py
+++ l10n_cn_standard_latest/__manifest__.py
@@ -9,7 +9,7 @@
 
 {
     'name': '2023最新中国会计科目表.企业标准会计.Latest Chinese Accounting for odoo16',
-    'version': '16.24.07.17',
+    'version': '16.24.09.06',
     'author': 'odooai.cn',
     'category': 'Accounting/Localizations/Account Charts',
     'website': 'https://www.odooai.cn',
