4954b76a2a9bea3f37970dc49dde42ea68397fbf
Ivan Office <guohuadeng@hotmail.com>
2025-11-22 06:47:15 +0800

    优化预收预付

--- l10n_cn_standard_latest/models/template_cn_standard.py
+++ l10n_cn_standard_latest/models/template_cn_standard.py
@@ -33,6 +33,7 @@ class AccountChartTemplate(models.AbstractModel):
             'property_account_income_categ_id': 'account_6001',
             'property_account_expense_categ_id': 'account_6401',
             'property_account_downpayment_categ_id': 'account_2203',
+            'property_account_prepay_categ_id': 'account_1123',
             'property_tax_receivable_account_id': 'account_2221_1_5',
             'property_tax_payable_account_id': 'account_2221_1_1',
             # 库存相关科目
