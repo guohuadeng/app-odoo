6e31e408717e464248dc40edab847fb94e822eba
Chill <chill421@qq.com>
2025-12-30 17:33:44 +0800

    app_module_superbar v19

--- app_module_superbar/__manifest__.py
+++ app_module_superbar/__manifest__.py
@@ -29,7 +29,7 @@
 
 {
     'name': "Apps Navigator by Category, Module Navigator",
-    'version': '19.0.24.11.12',
+    'version': '19.0.25.12.30',
     'author': 'odooai.cn',
     'category': 'Extra tools',
     'website': 'https://www.odooai.cn',
