2d5a2e9eeb1c5d16fbbf421eed257d39463ac272
Chill <chill421@qq.com>
2026-01-21 15:32:12 +0800

    [17]app_chatgpt国际化处理好 #IDKVB9

--- app_chatgpt/__manifest__.py
+++ app_chatgpt/__manifest__.py
@@ -10,7 +10,7 @@
 
 {
     'name': '!ChatGPT4, China Ali,AiGC Center.Ai服务中心，聚合全网Ai',
-    'version': '17.0.26.01.13',
+    'version': '17.0.26.01.21',
     'author': 'odooai.cn',
     'company': 'odooai.cn',
     'maintainer': 'odooai.cn',
