dc0bf79187e2f2e4d05a17457f3629a269ec9835
Ivan Office <guohuadeng@hotmail.com>
2026-01-22 22:17:27 +0800

    update track

--- app_common/__manifest__.py
+++ app_common/__manifest__.py
@@ -39,7 +39,7 @@
 
 {
     'name': "odooAi Common Util and Tools,欧度智能基础功能及面板",
-    'version': '18.0.25.09.12',
+    'version': '18.0.26.01.22',
     'author': 'odooai.cn',
     'category': 'Extra tools',
     'website': 'https://www.odooai.cn',
