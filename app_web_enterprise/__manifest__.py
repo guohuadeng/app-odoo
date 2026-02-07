1725a97cf9a642e48591ccf13a08d2e57eab9524
Ivan Office <guohuadeng@hotmail.com>
2026-02-07 19:54:15 +0800

    update ent

--- app_web_enterprise/__manifest__.py
+++ app_web_enterprise/__manifest__.py
@@ -17,7 +17,7 @@
 
 
 {
-    'name': "!odoo18 Enterprise enhance Pack,企业版界面及操作增强",
+    'name': "odoo18 Enterprise enhance Pack,企业版界面及操作增强",
     'version': '18.0.25.11.03',
     'author': 'odooai.cn',
     'category': 'Extra tools',
