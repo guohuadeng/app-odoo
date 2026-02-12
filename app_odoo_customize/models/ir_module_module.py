03f9e52879ac4daca79d3a7f5cf348d697b5caa5
Chill <chill421@qq.com>
2026-02-12 18:09:56 +0800

    prepare #IDQDQ8 app_odoo_customize增加将数据库设置为测试库的功能

--- app_odoo_customize/models/ir_module_module.py
+++ app_odoo_customize/models/ir_module_module.py
@@ -1,9 +1,14 @@
 # -*- coding: utf-8 -*-
 
+import operator
+import logging
+import os
+
 from odoo import api, fields, models, modules, tools, _
 from odoo.addons.base.models.ir_module import assert_log_admin_access
 
-import operator
+
+_logger = logging.getLogger(__name__)
 
 
 class IrModuleModule(models.Model):
@@ -34,24 +39,29 @@ class IrModuleModule(models.Model):
     def module_multi_refresh_po(self):
         lang = self.env.user.lang
         modules = self.filtered(lambda r: r.state == 'installed')
-        # 先清理, odoo原生经常清理不干净
-        # odoo 16中，不再使用 ir.translation，直接使用json字段
-        # for rec in modules:
-        #     translate = self.env['ir.translation'].search([
-        #         ('lang', '=', lang),
-        #         ('module', '=', rec.name)
-        #     ])
-        #     translate.sudo().unlink()
-        # 再重载
+        if not modules:
+            return {
+                'type': 'ir.actions.client',
+                'tag': 'display_notification',
+                'target': 'new',
+                'params': {
+                    'message': _("No installed modules selected."),
+                    'type': 'warning',
+                    'sticky': False,
+                    'next': {'type': 'ir.actions.act_window_close'},
+                }
+            }
+
         modules._update_translations(filter_lang=lang, overwrite=True)
-        # odoo 16翻译模式改变，仍需更新模块
+
+        self.env.registry.clear_cache('default', 'templates')
+
         return {
             'type': 'ir.actions.client',
             'tag': 'display_notification',
             'target': 'new',
             'params': {
-                'message': _("The languages that you selected have been successfully update.\
-                            You still need to Upgrade the apps to make it worked."),
+                'message': _("The languages that you selected have been successfully updated."),
                 'type': 'success',
                 'sticky': False,
                 'next': {'type': 'ir.actions.act_window_close'},
