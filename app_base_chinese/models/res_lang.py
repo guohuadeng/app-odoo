eddab79df2591b3651d361fd700890efa1cdb75a
Ivan Office <guohuadeng@hotmail.com>
2026-02-13 13:43:36 +0800

    优化chinese

--- app_base_chinese/models/res_lang.py
+++ app_base_chinese/models/res_lang.py
@@ -1,25 +1,14 @@
 # -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import fields, models
+from odoo import api, models, fields, _
+from odoo.exceptions import UserError, ValidationError
 
 
 class ResLang(models.Model):
     _inherit = 'res.lang'
-
-    def _get_date_format_selection(self):
-        current_year = fields.Date.today().year
-        return [
-            ('%Y-%m-%d', '%s-01-31' % current_year),
-            ('%d/%m/%Y', '01/31/%s' % current_year),
-            ('%m/%d/%Y', '31/01/%s' % current_year),
-            ('%Y/%m/%d', '%s/31/01' % current_year),
-            ('%d-%m-%Y', '01-31-%s' % current_year),
-            ('%m-%d-%Y', '31-01-%s' % current_year),
-            ('%Y-%m-%d', '%s-31-01' % current_year),
-        ]
-
-    time_format = fields.Selection(selection_add=[
-        ('%H:%M', '13:00'),
-        ('%I:%M:%S %p',)], ondelete={'%H:%M': 'set default'})
+    _order = 'active desc,sequence,name desc'
+    
+    sequence = fields.Integer('Sequence', help='Determine the display order', default=99)
+    time_format = fields.Selection(selection_add=[('%H:%M', '13:00')],
+        ondelete={'%H:%M': 'set default'})
 
