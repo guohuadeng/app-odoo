7f13e04fb12e3feed89cd47b8d3791c95c9e38f9
guohuadeng@hotmail.com <guohuadeng@hotmail.com>
2026-09-01 04:30:13 +0800

    fix(ir_http): 解决配置参数获取异常并完善默认值设置
    - 添加异常处理机制防止配置参数获取失败
    - 为所有应用配置项设置合理的默认值
    - 集成多语言支持并优化日志记录功能
    - 修复调试模式和订阅功能的默认配置问题
    - 更新文档链接和系统名称的默认设置

--- app_odoo_customize/models/ir_http.py
+++ app_odoo_customize/models/ir_http.py
@@ -1,31 +1,37 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+import logging
 
-from odoo import models
+from odoo import models, api, _
 from odoo.http import request
 
-
+_logger = logging.getLogger(__name__)
 
 class IrHttp(models.AbstractModel):
     _inherit = 'ir.http'
 
     def session_info(self):
         result = super(IrHttp, self).session_info()
-        config_parameter = request.env['ir.config_parameter'].sudo()
+        try:
+            config_parameter = request.env['ir.config_parameter'].sudo()
+        except Exception as e:
+            config_parameter = self.env['ir.config_parameter'].sudo()
+            _logger.warning("Error while getting config parameter: %s", e)
+            
         result['app_system_name'] = config_parameter.get_param('app_system_name', 'odooAi')
-        result['app_documentation_url'] = config_parameter.get_param('app_documentation_url')
-        result['app_documentation_dev_url'] = config_parameter.get_param('app_documentation_dev_url')
-        result['app_support_url'] = config_parameter.get_param('app_support_url')
-        result['app_account_title'] = config_parameter.get_param('app_account_title')
-        result['app_account_url'] = config_parameter.get_param('app_account_url')
-        result['app_show_lang'] = config_parameter.get_param('app_show_lang')
-        result['app_show_debug'] = config_parameter.get_param('app_show_debug')
-        result['app_show_documentation'] = config_parameter.get_param('app_show_documentation')
-        result['app_show_documentation_dev'] = config_parameter.get_param('app_show_documentation_dev')
-        result['app_show_support'] = config_parameter.get_param('app_show_support')
-        result['app_show_account'] = config_parameter.get_param('app_show_account')
-        result['app_show_poweredby'] = config_parameter.get_param('app_show_poweredby')
+        result['app_documentation_url'] = config_parameter.get_param('app_documentation_url', 'https://www.odooai.cn/r/yh18')
+        result['app_documentation_dev_url'] = config_parameter.get_param('app_documentation_dev_url', 'https://www.odooai.cn/r/kf18')
+        result['app_support_url'] = config_parameter.get_param('app_support_url', 'https://www.odooai.cn/trial')
+        result['app_account_title'] = config_parameter.get_param('app_account_title', _('我的Ai服务中心'))
+        result['app_account_url'] = config_parameter.get_param('app_account_url', 'https://www.odooai.cn/my')
+        result['app_show_lang'] = config_parameter.get_param('app_show_lang', True)
+        result['app_show_debug'] = config_parameter.get_param('app_show_debug', False)
+        result['app_show_documentation'] = config_parameter.get_param('app_show_documentation', True)
+        result['app_show_documentation_dev'] = config_parameter.get_param('app_show_documentation_dev', True)
+        result['app_show_support'] = config_parameter.get_param('app_show_support', True)
+        result['app_show_account'] = config_parameter.get_param('app_show_account', True)
+        result['app_show_poweredby'] = config_parameter.get_param('app_show_poweredby', False)
         # 增加多语言
         result['app_lang_list'] = self.env['res.lang'].search_read([], ['id', 'code', 'name', 'flag_image_url'])
         result['is_erp_manager'] = self.env.user.has_group('base.group_erp_manager')
@@ -33,7 +39,7 @@ class IrHttp(models.AbstractModel):
         result['app_navbar_pos_pc'] = config_parameter.get_param('app_navbar_pos_pc', 'top')
         result['app_navbar_pos_mobile'] = config_parameter.get_param('app_navbar_pos_mobile', 'top')
         # 此处直接取，不用 session
-        result['app_debug_only_admin'] = config_parameter.get_param('app_debug_only_admin')
-        result['app_stop_subscribe'] = config_parameter.get_param('app_stop_subscribe')
-        result['app_doc_root_url'] = config_parameter.get_param('app_doc_root_url', 'https://www.odoo.com')
+        result['app_debug_only_admin'] = config_parameter.get_param('app_debug_only_admin', True)
+        result['app_stop_subscribe'] = config_parameter.get_param('app_stop_subscribe', False)
+        result['app_doc_root_url'] = config_parameter.get_param('app_doc_root_url', 'https://www.odooai.cn')
         return result
