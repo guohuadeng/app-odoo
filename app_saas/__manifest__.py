b9a2fa7b39551d3157977f182d4d842c7e00d81b
Chill <chill421@qq.com>
2026-09-11 16:19:07 +0800

    refactor(ir_config): comment out error report URL record

--- app_saas/__manifest__.py
+++ app_saas/__manifest__.py
@@ -26,7 +26,7 @@
 
 {
     'name': "SaaS云服务客户端 / odooapp.cn SaaS Client - Ai Passport",
-    'version': '18.0.26.09.07',
+    'version': '18.0.26.09.11',
     'author': 'odooai.cn',
     'category': 'Base',
     'website': 'https://www.odooai.cn',
